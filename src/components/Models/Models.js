5a74b72a87e2418a8b7bb60371535edf4a8b8d3e
Keshav Mahawar <kes.mahawar@gmail.com>
2020-08-13 03:53:47 +0530

    DB: created slots and movies class functions

--- src/components/Models/Models.js
+++ src/components/Models/Models.js
@@ -1,137 +1,173 @@
-import movie from '../../JSON/movie.json'
-import categories from '../../JSON/categories.json'
-import bookings from '../../JSON/booking.json'
-import cinemas from '../../JSON/cinema.json'
-import slots from '../../JSON/slot.json'
+import movie from "../../JSON/movie.json";
+import categories from "../../JSON/categories.json";
+import bookings from "../../JSON/booking.json";
+import cinemas from "../../JSON/cinema.json";
+import slots from "../../JSON/slot.json";
 
-export class Movies{
-    constructor(){
-        this.movies = movie
-    }
+export class Movies {
+	constructor() {
+		let local = localStorage.getItem("movies");
+		this.moviesDB = local ? JSON.parse(local) : movie;
+	}
 
-    getMovies(){
-        return this.movies;
-    }
+	getMovies() {
+		const query = this.moviesDB.map(({ id, title, image, language, rating }) => {
+			let calculatedRating = (
+				rating.reduce((a, e) => a + e, 0) / rating.length
+			).toFixed(1);
 
-    inistialUpdateToAPI(){
-        // update the values to API.
-    }
+			return {
+				id,
+				title,
+				image,
+				language,
+				rating: calculatedRating,
+			};
+		});
 
-    getSlotsFromMovieIDnadLocation(){
-        // return slots based on movoe id and location
-    }
+		return query;
+	}
+	getMovieById(id) {
+		return this.moviesDB.find((movie) => movie.id == id);
+	}
 
+	inistialUpdateToAPI() {
+		// update the values to API.
+	}
+
+	getSlotsFromMovieIDnadLocation() {
+		// return slots based on movoe id and location
+	}
 }
 
-export class Slots{
-    constructor(){
-        this.slots = slots
-    }
+export class Slots {
+	constructor() {
+		let local = localStorage.getItem("slots");
+		this.slotsDB = local ? JSON.parse(local) : slots;
+	}
 
-    inistialUpdateToAPI(){
-        // update the values to API.
-    }
+	inistialUpdateToAPI() {
+		// update the values to API.
+	}
 
-    updateToAPI(){
-        // update the values to API.
-    }
+	updateToAPI() {
+		// update the values to API.
+	}
 
-    getAllSlotsBasedOnDate(date){
-        // return slots based on date - return from context API
-    }
+	getAllSlotsBasedOnDateLocationMovie(date, location, movie_id) {
+		// return slots based on date - return from context API
+		let groupSlots = {};
+		this.slotsDB.forEach((slot) => {
+			const cinema = new Cinema().getCinemaByID(slot.cinema_id);
+			const slotDate = new Date(slot.date);
+			let checkLine =
+				cinema.location === location &&
+				slot.movie_id === movie_id &&
+				new Date(date).getTime() === slotDate.getTime();
+			if (checkLine) {
+				let payload = { id: slot.id, time: slot.time };
+				if (groupSlots[cinema.theatre]) groupSlots[cinema.theatre].push(payload);
+				else groupSlots[cinema.theatre] = [payload];
+			}
+        });
+        //new Slots().getAllSlotsBasedOnDateLocationMovie('2020/08/14', "bangalore" ,2)
 
-    getAllSlotsOnAndAfterDate(date){
-        // return slots on and after the date - return from context API
-    }
+		return groupSlots;
+	}
 
-    getSlotsByID(id){
-        // return slots by ID - return from context API
-    }
+	getAllSlotsOnAndAfterDate(date) {
+		// return slots on and after the date - return from context API
+	}
 
-    modifySlotsSlotsByID(id){
-        // modify slots and return to API
-    }
+	getSlotsByID(id) {
+		// return slots by ID - return from context API
+	}
+
+	modifySlotsSlotsByID(id) {
+		// modify slots and return to API
+	}
 }
 
-export class Cinema{
-    constructor(){
-        this.cinemas = cinemas
-    }
+export class Cinema {
+	constructor() {
+		let local = localStorage.getItem("cinemas");
+		this.cinemasDB = local ? JSON.parse(local) : cinemas;
+	}
 
-    inistialUpdateToAPI(){
-        // update the values to API.
-    }
+	inistialUpdateToAPI() {
+		// update the values to API.
+	}
 
-    getAllCinemaFromLocation(location){
-        // return ciname based on location details
-    }
+	getAllCinemaFromLocation(location) {
+		// return ciname based on location details
+	}
 
-    getAllCinemas(){
-        return this.cinemas;
-    }
+	getAllCinemas() {
+		return this.cinemas;
+	}
 
-    getCinemaByID(id){
-        // return cinema based on id
-    }
+	getCinemaByID(id) {
+		return this.cinemasDB.find((cinema) => cinema.id == id);
+	}
 }
 
-export class Bookings{
-    constructor(){
-        this.bookings = bookings
-    }
+export class Bookings {
+	constructor() {
+		this.bookings = bookings;
+	}
 
-    inistialUpdateToAPI(){
-        // update the values to API.
-    }
+	inistialUpdateToAPI() {
+		// update the values to API.
+	}
 
-    getAllBookings(){
-        // needs to be extracted from Context API since booking details can change
-    }
+	getAllBookings() {
+		// needs to be extracted from Context API since booking details can change
+	}
 
-    getBookingFromID(id){
-        // get booking details based on ID -> this should not be extracted from context API
-    }
+	getBookingFromID(id) {
+		// get booking details based on ID -> this should not be extracted from context API
+	}
 
-    addBooking(id, slot_id, email, popcorn, price, seats){
-        // to be added to context API
-        // must update the slots seats based on slot ID. - see modifySlotsSlotsByID method.
-    }
+	addBooking(id, slot_id, email, popcorn, price, seats) {
+		// to be added to context API
+		// must update the slots seats based on slot ID. - see modifySlotsSlotsByID method.
+	}
 }
 
-export class Categories{
-    constructor(){
-        this.categories = categories
-    }
+export class Categories {
+	constructor() {
+		this.categories = categories;
+	}
 
-    inistialUpdateToAPI(){
-        // update the values to API.
-    }
+	inistialUpdateToAPI() {
+		// update the values to API.
+	}
 
-    getCategories(){
-        // return keys of categories - front, back, middle.
-    }
+	getCategories() {
+		// return keys of categories - front, back, middle.
+	}
 
-    getFrontMin(){
-        // return min value of the front row
-    }
+	getFrontMin() {
+		// return min value of the front row
+	}
 
-    getFrontMax(){
-        // return max value of the front row
-    }
+	getFrontMax() {
+		// return max value of the front row
+	}
 
-    getMiddleMin(){
-        // return min value of the middle row
-    }
+	getMiddleMin() {
+		// return min value of the middle row
+	}
 
-    getMiddleMax(){
-        // return max value of the middle row
-    }
+	getMiddleMax() {
+		// return max value of the middle row
+	}
 
-    getBackMin(){
-        // return min value of the back row
-    }
+	getBackMin() {
+		// return min value of the back row
+	}
 
-    getBackMax(){
-        // return minimum value of the back row
-    }
+	getBackMax() {
+		// return minimum value of the back row
+	}
 }
