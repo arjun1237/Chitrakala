88ca4e95d8abb4ced289db715389b08b59b3bfb4
Keshav Mahawar <kes.mahawar@gmail.com>
2020-08-12 14:09:49 +0530

    main styling theme

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import {StyledNavbar} from '../styling/styledComponents'
+import React from 'react';
+
+function Navbar() {
+  return (
+    <StyledNavbar>
+        <div className="logo">
+            ChitraKala
+        </div>
+    </StyledNavbar>
+  );
+}
+
+export default Navbar;
---
+++ src/styling/theme.js
@@ -0,0 +1,39 @@
+const themeMain = "#e7354b"; //red
+const themePrimary = "#0e80f8"; //blue
+const themeSuccess = "#4db78e"; //green
+const themeWarning = "#f1c346"; //yellow
+
+const textMain = "#222222"; //Text greyish Black
+const textSecondary = "rgb(102, 102, 102)"; //text light grey
+const textWhite = "white";
+const textPrimary = themePrimary; //blue
+const textSuccess = themeSuccess; //green
+const textWarning = themeWarning; //yellow
+
+const bgDark = "#212435"; //darkBlue
+const bgSecondary = "#f2f2f2"; //greyishBackground
+const bgWhite = "white";
+const bgPrimary = themePrimary; //blue
+const bgSuccess = themeSuccess; //green
+const bgWarning = themeWarning; //yellow
+
+const colors = {
+	themeMain,
+	themePrimary,
+	themeSuccess,
+	themeWarning,
+	textMain,
+	textSecondary,
+	textWhite,
+	textPrimary,
+	textSuccess,
+	textWarning,
+	bgDark,
+	bgSecondary,
+	bgWhite,
+	bgPrimary,
+	bgSuccess,
+	bgWarning,
+};
+
+export default colors
