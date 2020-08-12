10a53542d134b2919a2127ce327b6093c8b0bb4d
Keshav Mahawar <kes.mahawar@gmail.com>
2020-08-12 17:22:40 +0530

    merge conflict resolved

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Navbar from './components/Navbar'
 import LandingPage from './components/LandingPage/LandingPage'
-=======
-import Navbar from './components/Navbar';
->>>>>>> 79a4932adeb5037ebcb304a330e34f4f1b96f97c
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
         <Navbar />
         <LandingPage/>
-=======
-      <Navbar />
->>>>>>> 79a4932adeb5037ebcb304a330e34f4f1b96f97c
     </div>
   );
 }
